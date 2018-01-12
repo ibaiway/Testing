a5f1d72f30b3d73e5eba2a118098db33228fa110
Ibai <ik012982i9@206-06-HZ055279.H012982.NET>
2018-01-12 09:57:49 +0100

    Primer commit

---
+++ src/MyAssertEqualsTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+ 
+ 
+import static org.junit.Assert.*;
+ 
+public class MyAssertEqualsTest {
+ 
+    @Test
+    public void mySimpleEqualsTest(){
+         
+        String expectedName = "Nattu";
+        assertEquals(expectedName, Employee.getEmpNameWithHighestSalary());
+    }
+     
+    @Test
+    public void myObjectEqualsTest(){
+         
+        Employee expectedEmpObj = new Employee(1, "Nattu", 15000);
+        assertEquals(expectedEmpObj, Employee.getHighestPaidEmployee());
+    }
+}
